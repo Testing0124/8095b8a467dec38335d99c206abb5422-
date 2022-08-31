63486c5471171ee4d56a02ee528421dcb6a588c6
Testing0124 <93730370+Testing0124@users.noreply.github.com>
2022-08-31 10:33:47 -0300

    Create 8095b8a467dec38335d99c206abb5422.js

---
+++ export const elements = {      usuarios: '[href="/view/8095b8a467dec38335d99c206abb5422.js
@@ -0,0 +1,9 @@
+export const 8095b8a467dec38335d99c206abb5422 = {
+
+    usuarios: '[href="/view/users"]',
+    busca: 'input[type="text"]',
+    botoes: '.btn',
+    seleciona: ':checkbox',
+    test: '8095b8a467dec38335d99c206abb5422'
+    
+}
